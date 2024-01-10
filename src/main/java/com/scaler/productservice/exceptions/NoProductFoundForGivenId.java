f1415a1e9808192b445911b32b9108da832cf294
Sunny Vats <layers_tipsier_0n@icloud.com>
2024-01-10 21:08:37 +0530

    Add implementation of exception handler using ControllerAdvice annotation.

--- src/main/java/com/scaler/productservice/exceptions/NoProductFoundForGivenId.java
+++ src/main/java/com/scaler/productservice/exceptions/NoProductFoundForGivenId.java
@@ -1,5 +1,9 @@
 package com.scaler.productservice.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Given ID not found!")
 public class NoProductFoundForGivenId extends RuntimeException {
     public NoProductFoundForGivenId(String s) {
         super(s);
